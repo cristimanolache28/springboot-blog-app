a8a90eb5daea3a2870be41a37f7973f935039fd6
Cristian Manolache <cristianmanolache0@gmail.com>
2025-05-07 15:27:07 +0300

    configuration for aws cloud

--- src/main/java/com/blog/BlogAppSprigBootApplication.java
+++ src/main/java/com/blog/BlogAppSprigBootApplication.java
@@ -1,12 +1,16 @@
 package com.blog;
 
+import com.blog.entity.Role;
+import com.blog.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BlogAppSprigBootApplication {
+public class BlogAppSprigBootApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppSprigBootApplication.class, args);
@@ -17,4 +21,17 @@ public class BlogAppSprigBootApplication {
 		return new ModelMapper();
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("USER_ROLE");
+		roleRepository.save(userRole);
+	}
 }
